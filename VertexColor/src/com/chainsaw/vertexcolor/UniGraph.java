762a74b75e24f750598731262b89691977fd709b
Milenko Jovanovic <mile986@gmail.com>
2013-06-03 21:48:50 +0200

    First Commit

---
+++ VertexColor/src/com/chainsaw/vertexcolor/UniGraph.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.chainsaw.vertexcolor;
+
+import java.util.Iterator;
+
+/**
+ * @author Mile
+ *
+ */
+public class UniGraph<T> implements Iterable<T> {
+	
+	private final Map<T, Set<T>> = new HashMap<T, Set<t>>();
+	
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
