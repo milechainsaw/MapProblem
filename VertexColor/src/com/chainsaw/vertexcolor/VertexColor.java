0d799a4b0f1ab5e78027d437b65369e48a3b3406
Milenko Jovanovic <mile986@gmail.com>
2013-06-04 18:44:03 +0200

    Added graph minimization by removing duplicate connections

--- VertexColor/src/com/chainsaw/vertexcolor/VertexColor.java
+++ VertexColor/src/com/chainsaw/vertexcolor/VertexColor.java
@@ -2,6 +2,7 @@ package com.chainsaw.vertexcolor;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -12,10 +13,12 @@ public class VertexColor {
 	 */
 
 	public static void main(String[] args) {
-		loadFile("DATA/macedonia.txt");
-		
-		
-		
+		UniGraph<String> fullGraph = loadFile("DATA/macedonia.txt");
+
+		System.out.print("Data Loaded\n");
+		// Remove all duplicate connections
+		System.out.print(reduceGraph(fullGraph).toString());
+
 	}
 
 	private static UniGraph<String> loadFile(String _filename) {
@@ -25,21 +28,20 @@ public class VertexColor {
 			FileReader file = new FileReader(_filename);
 			BufferedReader input = new BufferedReader(file);
 			String line;
-			LinkedHashMap<String,String[]> vertexInput = new LinkedHashMap<String,String[]>();
-			while ((line = input.readLine())!=null) {
-				String parts[]=line.split(":");				
-				vertexInput.put(parts[0], parts[1].split(","));				
+			LinkedHashMap<String, String[]> vertexInput = new LinkedHashMap<String, String[]>();
+			while ((line = input.readLine()) != null) {
+				String parts[] = line.split(":");
+				vertexInput.put(parts[0], parts[1].split(","));
 			}
-			for(String node: vertexInput.keySet()){
+			for (String node : vertexInput.keySet()) {
 				graph.addNode(node);
 			}
-			for(Map.Entry<String, String[]> node: vertexInput.entrySet()){
-				for(String edge:node.getValue()){
+			for (Map.Entry<String, String[]> node : vertexInput.entrySet()) {
+				for (String edge : node.getValue()) {
 					graph.addEdge(node.getKey(), edge);
 				}
 			}
 			input.close();
-			
 
 		} catch (Exception e) {
 			System.out.print("Load Failed --> " + e);
@@ -48,4 +50,29 @@ public class VertexColor {
 		return graph;
 	}
 
+	private static UniGraph<String> reduceGraph(UniGraph<String> inGraph) {
+		UniGraph<String> minGraph = inGraph.clone();
+		Iterator<String> nodeIterator = minGraph.iterator();
+
+		while (nodeIterator.hasNext()) {
+			String node = nodeIterator.next();
+			try {
+				if (minGraph.getEdgeNodes(node) == null) {
+					nodeIterator.remove();
+				} else {
+					String[] edges = minGraph.getEdgeNodes(node).toArray(
+							new String[0]);
+					for (int i = 0; i < edges.length; i++) {
+						minGraph.removeEdge(edges[i], node);
+					}
+
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+		return minGraph;
+
+	}
 }
